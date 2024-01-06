57d1cd5920f4c7f845c132716f3589ebcfaf40b6
m410y <m410y@proton.me>
2024-01-06 23:48:05 +0700

    rk4 + torus connectivity

--- main.c
+++ main.c
@@ -25,19 +25,22 @@ typedef struct rank4 {
            yyxx, yyxy, yyyx, yyyy;
 } Rank4;
 
-typedef struct rank2_field {
+typedef struct rank2_field Rank2_field;
+struct rank2_field {
     Rank2 *F;
     size_t Nx, Ny;
     double dx, dy;
-} Rank2_field;
+    Rank1 (*conn)(Rank2_field *, Rank1 *, Rank2 **);
+};
 
-typedef struct particle_2d {
+typedef struct particle_2d Particle_2d;
+struct particle_2d {
     Rank1 pos;
     Rank1 vel;
     double time;
-} Particle_2d;
+};
 
-void rank2_field_alloc(Rank2_field *Field, size_t Nx, size_t Ny)
+void field_alloc(Rank2_field *Field, size_t Nx, size_t Ny)
 {
     if (Field->F)
         return;
@@ -50,9 +53,10 @@ void rank2_field_alloc(Rank2_field *Field, size_t Nx, size_t Ny)
     Field->Ny = Ny;
     Field->dx = 1.0 / Nx;
     Field->dy = 1.0 / Ny;
+    Field->conn = NULL;
 }
 
-void rank2_field_init(Rank2_field *Field, Rank2 (*func)(Rank1))
+void field_func_init(Rank2_field *Field, Rank2 (*func)(Rank1))
 {
     Rank1 point = {Field->dx / 2, Field->dy / 2};
     for (int i = 0; i < Field->Ny; i++)
@@ -83,7 +87,7 @@ Rank2 calc_metric(Rank1 point)
     return metric;
 }
 
-Rank2 rank2_inverse(Rank2 *M)
+Rank2 inverse(Rank2 *M)
 {
     double det = M->xx * M->yy - M->xy * M->yx;
     Rank2 res = {
@@ -96,7 +100,7 @@ Rank2 rank2_inverse(Rank2 *M)
     return res;
 }
 
-Rank1 nearest_points(Rank2_field *Field, Rank1 *P, Rank2 *elems[4])
+Rank1 torus_conn(Rank2_field *Field, Rank1 *P, Rank2 *elems[4])
 {
     int j = P->x / Field->dx - 0.5;
     int i = P->y / Field->dy - 0.5;
@@ -115,10 +119,13 @@ Rank1 nearest_points(Rank2_field *Field, Rank1 *P, Rank2 *elems[4])
     int jp = (j + 1) % Field->Nx;
     int ip = (i + 1) % Field->Ny;
 
-    elems[0] = &Field->F[i * Field->Nx + j];
-    elems[1] = &Field->F[i * Field->Nx + jp];
-    elems[2] = &Field->F[ip * Field->Nx + j];
-    elems[3] = &Field->F[ip * Field->Nx + jp];
+    if (elems)
+    {
+        elems[0] = &Field->F[i * Field->Nx + j];
+        elems[1] = &Field->F[i * Field->Nx + jp];
+        elems[2] = &Field->F[ip * Field->Nx + j];
+        elems[3] = &Field->F[ip * Field->Nx + jp];
+    }
 
     return Pp;
 }
@@ -126,7 +133,7 @@ Rank1 nearest_points(Rank2_field *Field, Rank1 *P, Rank2 *elems[4])
 Rank2 value_at_point(Rank2_field *Field, Rank1 P)
 {
     Rank2 *elems[4];
-    P = nearest_points(Field, &P, elems);
+    P = Field->conn(Field, &P, elems);
 
     double coefs[4];
     coefs[3] = P.x * P.y;
@@ -146,7 +153,7 @@ Rank2 value_at_point(Rank2_field *Field, Rank1 P)
 Rank3 diff_at_point(Rank2_field *Field, Rank1 P)
 {
     Rank2 *elems[4];
-    P = nearest_points(Field, &P, elems);
+    P = Field->conn(Field, &P, elems);
 
     double coefs_x[4];
     coefs_x[3] = P.y / Field->dx;
@@ -186,7 +193,7 @@ Rank3 christoffel_at_point(Rank2_field *Field, Rank1 P)
                     ((double *)&diff)[4 * k + 2 * j + i]);
 
     Rank2 inv = value_at_point(Field, P);
-    inv = rank2_inverse(&inv);
+    inv = inverse(&inv);
 
     Rank3 res;
     memset(&res, 0, sizeof(Rank3));
@@ -199,49 +206,70 @@ Rank3 christoffel_at_point(Rank2_field *Field, Rank1 P)
     return res;
 }
 
-void particle_init(Particle_2d *particle, const char* filename)
+void particle_init(Particle_2d *particle, FILE *const file)
 {
-    FILE *const file = fopen(filename, "r");
-
     fscanf(file, "%lf %lf", &particle->pos.x, &particle->pos.y);
     fscanf(file, "%lf %lf", &particle->vel.x, &particle->vel.y);
 
     particle->time = 0;
-
-    fclose(file);
 }
 
-void move_particle(Rank2_field *Field, Particle_2d *particle)
+Rank1 particle_acceleration(Rank2_field *Field, const Rank1 *P, const Rank1 *V)
 {
-    Rank3 christ = christoffel_at_point(Field, particle->pos);
-
-    const double ds = 1.0;
-    const double *u = (const double *)&particle->vel;
+    Rank3 christ = christoffel_at_point(Field, *P);
+    const double *u = (const double *)V;
     Rank1 du = {0, 0};
     for (int i = 0; i < 2; i++)
         for (int j = 0; j < 2; j++)
             for (int k = 0; k < 2; k++)
                 ((double *)&du)[i] -=
-                    ((double *)&christ)[4 * i + 2 * j + k] * u[k] * u[j];
+                    ((double *)&christ)[4 * i + 2 * j + k] * u[j] * u[k];
 
-    particle->pos.x += particle->vel.x * ds;
-    if (particle->pos.x > 1.0)
-    {
-        particle->pos.x = 2.0 - particle->pos.x;
-        particle->vel.x = - particle->vel.x;
-    }
-    if (particle->pos.x < 0.0)
-    {
-        particle->pos.x = - particle->pos.y;
-        particle->vel.x = - particle->vel.x;
-    }
-    particle->pos.y += particle->vel.y * ds;
-    if (particle->pos.y > 1.0)
-        particle->pos.y -= 1.0;
-    if (particle->pos.y < 0.0)
-        particle->pos.y += 1.0;
-    particle->vel.x += du.x * ds;
-    particle->vel.y += du.y * ds;
+    return du;
+}
+
+void move_particle(Rank2_field *Field, Particle_2d *particle, double ds)
+{
+    Rank1 P = particle->pos;
+    Rank1 V = particle->vel;
+
+    Rank1 dP_1 = V;
+    Rank1 dV_1 = particle_acceleration(Field, &P, &V);
+
+    P.x = particle->pos.x + dP_1.x * ds / 2;
+    P.y = particle->pos.y + dP_1.y * ds / 2;
+    V.x = particle->vel.x + dV_1.x * ds / 2;
+    V.y = particle->vel.y + dV_1.y * ds / 2;
+
+    Rank1 dP_2 = V;
+    Rank1 dV_2 = particle_acceleration(Field, &P, &V);
+
+    P.x = particle->pos.x + dP_2.x * ds / 2;
+    P.y = particle->pos.y + dP_2.y * ds / 2;
+    V.x = particle->vel.x + dV_2.x * ds / 2;
+    V.y = particle->vel.y + dV_2.y * ds / 2;
+
+    Rank1 dP_3 = V;
+    Rank1 dV_3 = particle_acceleration(Field, &P, &V);
+
+    P.x = particle->pos.x + dP_3.x * ds;
+    P.y = particle->pos.y + dP_3.y * ds;
+    V.x = particle->vel.x + dV_3.x * ds;
+    V.y = particle->vel.y + dV_3.y * ds;
+
+    Rank1 dP_4 = V;
+    Rank1 dV_4 = particle_acceleration(Field, &P, &V);
+
+    P.x = particle->pos.x + (dP_1.x + 2 * dP_2.x + 2 * dP_3.x + dP_4.x) * ds / 6;
+    P.y = particle->pos.y + (dP_1.y + 2 * dP_2.y + 2 * dP_3.y + dP_4.y) * ds / 6;
+    V.x = particle->vel.x + (dV_1.x + 2 * dV_2.x + 2 * dV_3.x + dV_4.x) * ds / 6;
+    V.y = particle->vel.y + (dV_1.y + 2 * dV_2.y + 2 * dV_3.y + dV_4.y) * ds / 6;
+
+    P.x -= floor(P.x);
+    P.y -= floor(P.y);
+
+    particle->pos = P;
+    particle->vel = V;
 
     particle->time += ds;
 }
@@ -257,27 +285,39 @@ int main(int argc, char *argv[])
 
     Rank2_field metric_field;
     Particle_2d tester;
+    int iterations;
+    double step;
+    int Nx, Ny;
+    FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w");;
+
+    fscanf(in, "%d", &iterations);
+    fscanf(in, "%lf", &step);
+    fscanf(in, "%d%d", &Nx, &Ny);
 
     Rank2_field *Field = &metric_field;
     memset(Field, 0, sizeof(Rank2_field));
-    rank2_field_alloc(Field, 10, 10);
-    rank2_field_init(Field, calc_metric);
+    field_alloc(Field, Nx, Ny);
+    field_func_init(Field, calc_metric);
+    Field->conn = torus_conn;
 
     Particle_2d *particle = &tester;
     memset(particle, 0, sizeof(Particle_2d));
-    particle_init(particle, argv[1]);
+    particle_init(particle, in);
 
-    FILE *out = fopen(argv[2], "w");
+    fclose(in);
+
     fprintf(out, "%lf\t%lf\t%lf\t%lf\t%lf\n", particle->time,
             particle->pos.x, particle->pos.y,
             particle->vel.x, particle->vel.y);
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < iterations; i++)
     {
-        move_particle(Field, particle);
+        move_particle(Field, particle, step);
         fprintf(out, "%lf\t%lf\t%lf\t%lf\t%lf\n", particle->time,
                 particle->pos.x, particle->pos.y,
                 particle->vel.x, particle->vel.y);
     }
+
     fclose(out);
 
     delete_field(Field);
--- plot_script.p
+++ plot_script.p
@@ -2,5 +2,5 @@ set terminal png size 500, 500
 set output 'grav.png'
 set xrange[0:1]
 set yrange[0:1]
-set title 'Sphere'
-plot 'output.txt' using 2:3 with lines
+set title 'Torus'
+plot 'output.txt' using 2:3 with dots
